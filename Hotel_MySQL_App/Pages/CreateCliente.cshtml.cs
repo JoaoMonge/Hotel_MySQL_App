920acf5cadff6d202dbc181f082349cf8254c498
João Monge <joao.monge13@gmail.com>
2023-02-07 11:47:54 +0000

    Alterações adicionado Cliente

---
+++ Hotel_MySQL_App/Pages/CreateCliente.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel_MySQL_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_MySQL_App.Pages
+{
+	public class CreateHotelModel : PageModel
+    {
+
+
+        public void OnGet()
+        {
+
+           
+
+        }
+
+        public void OnPost()
+        {
+            Hotel hotel = new Hotel();
+            hotel.Sigla_Hotel = Request.Form["sigla"];
+            hotel.Designacao = Request.Form["designacao"];
+            hotel.Localizacao = Request.Form["localizacao"];
+
+            HoteisContext context = new HoteisContext();
+
+            context.createHotel(hotel);
+                
+
+            OnGet();
+        }
+    }
+}
